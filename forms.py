0f395d67d7578e7e322a7c16b2315cfded4dbd56
blazejosojca <blazejosojca@gmail.com>
2018-10-07 01:21:15 +0200

    layout of login and register pages updated

--- forms.py
+++ forms.py
@@ -8,8 +8,8 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     password_repeat = PasswordField('Confirm Password',
-                                    validators=[DataRequired(), EqualTo(password)])
-    submit = SubmitField('Sign UP')
+                                    validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
