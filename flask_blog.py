f3f9e3c569108a41f1d16d484f935273d62df0ec
blazejosojca <blazejosojca@gmail.com>
2018-10-07 01:21:15 +0200

    layout of login and register pages updated

--- flask_blog.py
+++ flask_blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from config_dir.config import keys
 from forms import RegistrationForm, LoginForm
 
@@ -32,10 +32,15 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/register")
+
+@app.route("/register", methods=['GET','POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"A new user: {form.username.data}added. Congratulations!", 'success')
+        return  redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
+
 
 @app.route("/login")
 def login():
