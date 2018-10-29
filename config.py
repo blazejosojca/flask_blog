5271b7295dbcf833236c20b90602df2efb449ff5
blazejosojca <blazejosojca@gmail.com>
2018-10-29 22:54:36 +0100

    changes in structure and packages

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard_to_guess_12345678'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
