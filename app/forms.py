14312b6c629b053be3974c6d446d3bd2306756f9
blazejosojca <blazejosojca@gmail.com>
2018-11-05 23:33:37 +0100

    fixed small error

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -12,6 +13,18 @@ class RegistrationForm(FlaskForm):
                                             validators=[DataRequired(),
                                             EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('This username already exists. Please use a different username!')
+
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('This email already exists. Please use a different email!')
 
 
 class LoginForm(FlaskForm):
