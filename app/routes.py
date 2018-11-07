bfe06e837f3cd2354ad730fa59dfc04a2172c062
blazejosojca <blazejosojca@gmail.com>
2018-11-08 00:27:44 +0100

    authentication completed ;-)

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import (render_template,
                    flash,
                    request,
                    redirect)
+
 from werkzeug.urls import url_parse
 from flask_login import (current_user,
                          login_user,
@@ -61,18 +62,18 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('/home'))
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash("Invalid username or password")
+            flash('Credentials are incorrect!', 'warning')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('login')
-        return redirect(url_for('home'))
+            next_page = url_for('home')
+        return redirect(next_page)
     return render_template('login.html', title='Login', form=form)
 
 
@@ -80,3 +81,8 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('about'))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Account')
