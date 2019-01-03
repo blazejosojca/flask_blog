fe8a3f16aadcdc079a9e144647ac32bcdf7647b3
blazejosojca <blazejosojca@gmail.com>
2019-01-04 00:31:28 +0100

    logging system added - logging to file

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
+import os
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from config import *
+from config import Config
 
 
 app = Flask(__name__)
@@ -14,4 +17,18 @@ login.login_view = 'login'
 login.login_message_category = 'info'
 
 from app import routes, models, errors
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/flask_blog', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+                            ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Flask blog')
+
 
--- config.py
+++ config.py
@@ -5,13 +5,21 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     ENV = 'development'
     TESTING = False
-    DEBUG = True
+    DEBUG = False
     CSRF_ENABLED = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard_to_guess_12345678'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     #mail configuration for sending logs
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = str(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['test@mail.com']
+
+    #TODO - finish configuration for email logging notifications
 
 
 class DevelopmentConfig(Config):
