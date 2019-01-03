914fdf7718127e9faecefdb8908d017a3518335c
blazejosojca <blazejosojca@gmail.com>
2019-01-03 23:35:10 +0100

    improved info about config -new values ENV which describes the cuurent ENV

--- config.py
+++ config.py
@@ -3,19 +3,24 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    ENV = 'development'
     TESTING = False
-    DEBUG = False
+    DEBUG = True
     CSRF_ENABLED = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard_to_guess_12345678'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    #mail configuration for sending logs
+
 
 class DevelopmentConfig(Config):
+    DEVELOPMENT = True
     DEBUG = True
 
 
 class ProductionConfig(Config):
+    ENV = 'production'
     DEBUG = False
 
 
@@ -25,5 +30,6 @@ class StagingConfig(Config):
 
 
 class TestConfig(Config):
+    DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.db')
     TESTING = True
