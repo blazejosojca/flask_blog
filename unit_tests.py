e0f04822be6132aa5c685adc15bcaf474191feed
blazejosojca <blazejosojca@gmail.com>
2019-02-15 22:00:39 +0100

    modified unittests after blueprints adding

--- unit_tests.py
+++ unit_tests.py
@@ -1,35 +1,38 @@
 import unittest
 import os
-from flask import url_for
+from flask import url_for, current_app
 
 from flask_testing import TestCase
 
-from app import app, db
-from config import basedir
+from app import create_app, db
+from config import basedir, Config
 from app.models import User, Post
-
-from app.auth import routes
-from app.main import routes
-from app.posts import routes
-from app.errors import routes
 
 TEST_USER_NAME = 'test_user'
 TEST_USER_EMAIL = 'test@mail.com'
 TEST_PASSWORD = 'password'
 
 
+class TestConfig(Config):
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+
 class BaseTest(TestCase):
-    # Base methods for starting and closing/ cleaning testing   enviroment
+
     def create_app(self):
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
+        app = create_app(TestConfig)
+
         return app
 
     def setUp(self):
-        self.app = app.test_client()
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        self.app = current_app.test_client()
+
         db.create_all()
-        user = User(username=TEST_USER_NAME, email=TEST_USER_EMAIL)
+
+        user = User(username=TEST_USER_NAME,
+        email=TEST_USER_EMAIL)
         user.set_password('password')
         post = Post(title='test title', content='test content', author=user)
         db.session.add(user)
@@ -42,18 +45,18 @@ class BaseTest(TestCase):
 
     # Tests for ensure that flask is set up
     def test_server_is_up(self):
-        tester = app.test_client(self)
-        response = tester.get("/", content_type='html/text')
+        tester = current_app.test_client(self)
+        response = tester.get(url_for('main.home'), content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
     def test_database(self):
         tester = os.path.exists(os.path.join(basedir, 'test.db'))
         self.assertTrue(tester)
-    #
+
     # Base test methods for further using
     def register(self, username, email, password, password_confirmation):
         return self.app.post(
-            '/register',
+            '/auth/register',
             data=dict(
                 username=username,
                 email=email,
@@ -63,23 +66,12 @@ class BaseTest(TestCase):
 
     def login(self, email, password):
         return self.app.post(
-            '/login',
+            '/auth/login',
             data=dict(email=email, password=password), follow_redirects=True)
 
     def logout(self):
         return self.app.get('/logout', follow_redirects=True)
 
-
-class TestMainApp(BaseTest):
-
-    def test_server_is_up(self):
-        tester = app.test_client(self)
-        response = tester.get("/", content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-
-    def test_database(self):
-        tester = os.path.exists(os.path.join(basedir, 'test.db'))
-        self.assertTrue(tester)
 
 class TestModels(BaseTest):
 
@@ -94,56 +86,61 @@ class TestModels(BaseTest):
         self.assertEqual(User.query.count(), 2)
 
     def test_post_model(self):
-        user=User.query.filter_by(username='test_user').first()
-
-        post1=Post(title='test title', content='test content', author=user)
+        user = User.query.filter_by(username='test_user').first()
+        post1 = Post(title='test title',
+                     content='test content',
+                     author=user)
         db.session.add(post1)
         db.session.commit()
 
         self.assertEqual(Post.query.count(), 2)
 
     def test_adding_new_post(self):
-        user=User.query.filter_by(username='test_user').first()
-        post1=Post(title='test title', content='test content', author=user)
+        user = User.query.filter_by(username='test_user').first()
+        post1 = Post(title='test title',
+                     content='test content',
+                     author=user)
         db.session.add(post1)
         db.session.commit()
 
         self.assertEqual(Post.query.count(), 2)
 
+
 class TestRoutes(BaseTest):
 
     # Tests of loading crucial pages
     def test_home_page_loads(self):
-        response = self.app.get('/bp', follow_redirects=True)
+        response = self.app.get(url_for('main.home'), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed('bp.html')
+        self.assertTemplateUsed('main/home.html')
 
     def test_login_page_loads(self):
-        tester = app.test_client(self)
-        response = tester.get('/login', content_type='html/text')
-        self.assertTrue(b'Login Page' in response.data)
+        response = self.app.get(url_for('auth.login'), content_type='html/text')
+        self.assertTrue(b'Log In!' in response.data)
 
     def test_about_page_loads(self):
-        response = self.app.get('/about', follow_redirects=True)
+        response = self.app.get(url_for('main.about'), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed('about.html')
+        self.assertTemplateUsed('main/about.html')
 
     def test_register_page(self):
-        response = self.app.get('/register', follow_redirects=True)
+        response = self.app.get(url_for('auth.register'), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed('register.html')
+        self.assertTemplateUsed('auth/register.html')
 
     def test_login_page(self):
-        response = self.app.get('/login', follow_redirects=True)
+        response = self.app.get(url_for('auth.login'), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed('login.html')
+        self.assertTemplateUsed('auth/login.html')
 
     def test_logout_page(self):
-        response = self.app.get('/logout', follow_redirects=True)
+        response = self.app.get(url_for('auth.logout'), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+
 
 class TestDisplayingTemplates(BaseTest):
     pass
+
 
 class TestLoginRoute(BaseTest):
 
@@ -152,8 +149,9 @@ class TestLoginRoute(BaseTest):
         self.assertIn(b'You were logged in!', response.data)
 
     def test_login_page_with_empty_credentials(self):
-        response = self.login('','')
+        response = self.login('', '')
         self.assertIn(b'Log In!', response.data)
+
 
 class TestRegistrationRoute(BaseTest):
 
@@ -164,6 +162,7 @@ class TestRegistrationRoute(BaseTest):
     def test_registration_with_valid_data(self):
         response = self.register("new_test", 'new_test@mail.com', TEST_PASSWORD, TEST_PASSWORD)
         self.assertIn(b'Login Page', response.data)
+
 
 if __name__ == '__main__':
     unittest.main()
