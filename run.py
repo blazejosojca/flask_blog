14874c759c55d9bcc24e0fc30b7d4c547541d92d
blazejosojca <blazejosojca@gmail.com>
2019-04-15 23:17:03 +0200

    updated templates and added cli for translations

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
-from app import db, create_app
+from app import db, create_app, cli
 from app.models import User, Post
-from flask_script import Manager
-import unittest
 
 
 app = create_app()
+cli.register(app)
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
 
 if __name__ == '__main__':
     app.run()
